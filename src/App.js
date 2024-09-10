30c5dd32fd560505ae5ac79e01968106ff32fb98
Tanya Arora <tanyaa.ar28@gmail.com>
2024-09-10 17:53:00 +0530

    resolve Merge conflict

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import "./App.css";
-import HomePage from "./components/HomePage";
-import ManageBills from "./components/ManageBills";
-import AddBills from "./components/AddBills";
-import backgroundImage from "./images/capstone-bg-img.jpg";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-import BillsOverview from "./components/BillsOverview";
+import './App.css';
+import HomePage from './components/HomePage';
+import ManageBills from './components/ManageBills';
+import AddBills from './components/AddBills';
+import backgroundImage from './images/capstone-bg-img.jpg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import BillsOverview from './components/BillsOverview';
+import BillsOverviewDetails from './components/BillsOverviewDetails';
+import BillDetailsPage from './components/BillDetailsPage';
 import OverdueUpcoming from "./components/OverdueUpcoming";
 import PaymentPage from "./components/PaymentPage";
 import "bootstrap/dist/css/bootstrap.css";
 import OverdueBill from "./components/OverdueBill";
 import UpcomingBill from "./components/UpcomingBill";
+
 function App() {
   return (
     <Router>
-      <AnimatePresence mode="wait">
+      <AnimatePresence mode='wait'>
         <div className="App">
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/manageBills" element={<ManageBills />} />
-            <Route path="/manageBills/addBills" element={<AddBills />} />
-            <Route
-              path="/manageBills/billsOverview"
-              element={<BillsOverview />}
-            />
+            <Route path='/' element={<HomePage />} />
+            <Route path='/manageBills' element={<ManageBills />} />
+            <Route path='/manageBills/addBills' element={<AddBills />} />
+            <Route path='/manageBills/billsOverview' element={<BillsOverview />} />
+            <Route path='/manageBills/billsOverview/allDetails' element={<BillsOverviewDetails />} />
+            <Route path='/manageBills/billsOverview/:category' element={<BillDetailsPage />} />
             <Route
               path="/manageBills/overdueUpcoming"
               element={<OverdueUpcoming />}
@@ -46,5 +48,6 @@ function App() {
     </Router>
   );
 }
+
 
 export default App;
