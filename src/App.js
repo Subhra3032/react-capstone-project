1f4e9482a42e519c812ee3fd4e937c0ae902580d
Tanya Arora <tanyaa.ar28@gmail.com>
2024-09-10 18:00:58 +0530

    Resolve imports

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import backgroundImage from './images/capstone-bg-img.jpg';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import BillsOverview from './components/BillsOverview';
-import BillsOverviewDetails from './components/BillsOverviewDetails';
-import BillDetailsPage from './components/BillDetailsPage';
 import OverdueUpcoming from "./components/OverdueUpcoming";
 import PaymentPage from "./components/PaymentPage";
 import "bootstrap/dist/css/bootstrap.css";
@@ -24,8 +22,6 @@ function App() {
             <Route path='/manageBills' element={<ManageBills />} />
             <Route path='/manageBills/addBills' element={<AddBills />} />
             <Route path='/manageBills/billsOverview' element={<BillsOverview />} />
-            <Route path='/manageBills/billsOverview/allDetails' element={<BillsOverviewDetails />} />
-            <Route path='/manageBills/billsOverview/:category' element={<BillDetailsPage />} />
             <Route
               path="/manageBills/overdueUpcoming"
               element={<OverdueUpcoming />}
